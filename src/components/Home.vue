3dd7b7ef5f457b0eb0a05bee4a76b5ae4a134f9d
Gabriel <utilateurmacbookpro@MacBook-Pro-de-Monsieur.local>
2019-07-20 22:57:32 +0200

    Ui integration

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,27 @@
+<template>
+  <div class='home-content'>
+    <h1>Jokes de Papa</h1>
+    <div class='content'>
+      <img src='../assets/arrow.svg' alt='before arrow' class='before' v-on:click='count > 0 ? count -= 1: null'>
+      <JokeComponent
+        :joke='jokes[count]'
+      />
+      <img src='../assets/arrow.svg' alt='next arrow' class='next' v-on:click='count+1 < jokes.length ? count += 1: null'>
+    </div>
+    <div class='btns'>
+      <div class='btn'>
+        A rigolé
+      </div>
+      <div class='btn'>
+        N'a pas ri
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
+import JokeComponent from './JokeComponent.vue'
+
 const firebase = require('firebase')
 // Required for side-effects
 var config = {
@@ -12,10 +35,29 @@ var database = firebase.database()
 console.log(database)
 
 export default {
-  name: 'HelloWorld',
+  components: {
+    JokeComponent
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      count: 1,
+      jokes: [
+        {
+          content: 'toto',
+          answer: 'mlkmlk',
+          score: 100
+        },
+        {
+          content: 'bdfkljsdfkljgs lljkdfsghdl k',
+          answer: '',
+          score: 100
+        },
+        {
+          content: 'sdfjkghldjkh sldjkldjkf',
+          answer: 'ljklj ljkh lkjh lkj',
+          score: 100
+        }
+      ]
     }
   },
   methods: {
@@ -33,47 +75,75 @@ export default {
 }
 </script>
 
-<template>
-  <div class="home-content">
-    <h1>Jokes de Papa</h1>
-    <p>Quel est la diff entre ta mère et un avion de chasse ?</p>
-    <div class="btns">
-      <div class='btn'>
-        A rigolé
-      </div>
-      <div class='btn'>
-        N'a pas ri
-      </div>
-    </div>
-  </div>
-</template>
-
 <style scoped>
-.home-content{
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-}
+  .home-content{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
 
-h1, p{
-  text-align: center;
-}
+  h1{
+    text-align: center;
+    font-size: 35px;
+    font-family: 'Kadisoka Script';
+    color: #CF5858;
+  }
 
-.btns{
-  display: flex;
-  justify-content: center;
-}
+  .content{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 45vh;
+  }
 
-.btn{
-  margin: 0 10px;
-  width: 180px;
-  box-sizing: border-box;
-  text-align: center;
-  padding: 15px;
-  background: blue;
-  color: white;
-  border-radius: 6px;
-  height: max-content;
-}
+  .before, .next{
+    width: 50px;
+    margin: 0 50px;
+  }
+
+  .before:active, .next:active{
+    margin: 0 55px;
+    width: 40px;
+  }
+
+  .before{
+    transform: rotate(180deg);
+  }
+
+  .btns{
+    display: flex;
+    justify-content: center;
+    height: 80px;
+  }
+
+  .btn{
+    margin: 0 10px;
+    width: 180px;
+    box-sizing: border-box;
+    text-align: center;
+    padding: 15px;
+    background: blue;
+    color: white;
+    border-radius: 10px;
+    height: max-content;
+    user-select: none;
+    cursor: pointer;
+  }
+
+  .btn:first-child{
+    background-color: #FF5858;
+    box-shadow: 0 5px 0 0 rgb(239, 63, 63, 100);
+  }
+
+  .btn:last-child{
+    background-color: #27AE60;
+    box-shadow: 0 5px 0 0 rgba(28, 144, 76, 100);
+  }
+
+  .btn:active{
+    margin-top: 5px;
+    box-shadow: 0 0px 0 0 rgba(0, 0, 0, 0);
+    transition: all ease .1s;
+  }
 </style>
---
+++ src/components/JokeComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <p class="content">{{ joke.content }}</p>
+    <div v-if='joke.answer.length > 0' class="answer content">
+      <div class="answer-mask">
+        <p>Réponse</p>
+      </div>
+      {{ joke.answer }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    joke: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .content{
+    text-align: center;
+    font-size: 42px;
+    color: white;
+    font-family: 'Open Sans';
+    font-weight: 300;
+    font-style: italic;
+  }
+
+  .answer{
+    margin: 20px auto 0 auto;
+    width: max-content;
+    padding: 7px 20px;
+    position: relative;
+    color: transparent;
+    transition: all ease .2s;
+  }
+
+  .answer-mask{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border-radius: 10px;
+    border: dashed 3px rgba(162, 60, 60, 0.5);
+    font-size: 24px;
+    opacity: 1;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .answer-mask:hover{
+    opacity: 0;
+  }
+
+  .answer:hover{
+    color: white;
+  }
+
+  .answer-mask p{
+    color: rgba(162, 60, 60, 0.76);
+    text-align: center;
+    line-height: 290%;
+  }
+</style>
